56d337962501eed3b9240e39e302650bfa787fe7
tywenrick3 <tew287@nyu.edu>
2022-04-29 16:09:29 -0400

    update

---
+++ components/FitnessNode.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, SafeAreaView} from 'react-native';
+import React from 'react';
+
+const FitnessNode = () => {
+  return (
+    <SafeAreaView>
+      <Text>FitnessNode</Text>
+    </SafeAreaView>
+  );
+};
+
+export default FitnessNode;
+
+const styles = StyleSheet.create({});
